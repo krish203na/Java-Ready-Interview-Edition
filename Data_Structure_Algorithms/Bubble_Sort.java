becd33dc79bccbdcd03af56c95fdfd39ef0290e0
krish203na <kanhapanchal03@gmail.com>
2024-02-22 20:26:54 +0530

    updated bubble sort

--- Data_Structure_Algorithms/Bubble_Sort.java
+++ Data_Structure_Algorithms/Bubble_Sort.java
@@ -5,6 +5,9 @@ public class Bubble_Sort {
 
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
+            while (i>5) {
+                
+            }
 
         }
     }
